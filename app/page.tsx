5b64c3d6ea5ae9dfd1c834093cc1cbabc131b636
Bryan Murasira <bryanmurasira@gmail.com>
2024-02-16 23:44:10 +0200

    ch: added plan for main page

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,9 @@ import Tasks from "./components/Task";
 export default function Home() {
   return (
     <main className="">
+      {/* Navigation bar */}
       <Tasks />
+      {/* footer */}
     </main>
   );
 }
