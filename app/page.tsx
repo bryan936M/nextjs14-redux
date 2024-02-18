9f1f0ccd4ca7f90b89dd6d6640566605356fd1a5
Bryan Murasira <bryanmurasira@gmail.com>
2024-02-18 16:42:41 +0200

    feat: completed full integration with redux

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,39 @@
 'use client'
-import { FormEvent } from "react";
+import { FormEvent, useState } from "react";
 import Tasks from "../components/Task";
+import { useAppDispatch, useAppSelector } from "../redux/hooks";
+import { selectTasks, taskActions } from "@/redux/features/taskSlice";
+import { it } from "node:test";
 
 export default function Home() {
+  const [taskTitle, setTaskTitle] = useState<string>("");
+  const [taskDesc, setTaskDesc] = useState<string>("");
+  const dispatch = useAppDispatch();
+  const { items, error } = useAppSelector(selectTasks);
+
   function handleSubmit(event: FormEvent<HTMLFormElement>): void {
-    throw new Error("Function not implemented.");
+    event.preventDefault();
+    if (items.length === 0) {
+      dispatch(
+        taskActions.addTask({
+          id: 1,
+          title: taskTitle,
+          description: taskDesc,
+          isCompleted: false,
+        })
+      );
+
+      return;
+    }
+    const lastTask = items[items.length - 1];
+    dispatch(
+      taskActions.addTask({
+        id: lastTask.id + 1,
+        title: taskTitle,
+        description: taskDesc,
+        isCompleted: false,
+      })
+    );
   }
 
   return (
@@ -13,15 +42,28 @@ export default function Home() {
       <form onSubmit={handleSubmit} className="w-1/2 mx-auto space-y-2">
         <h1 className="font-extrabold text-2xl text-slate-200">Task</h1>
         <div className="flex space-x-3">
-          <input
-            type="text"
-            name="task_input"
-            id="task_input"
-            className="bg-slate-600 w-3/4 h-full p-3 rounded-lg text-2xl text-slate-300"
-          />
+          <div className="flex space-x-3">
+            <input
+              type="text"
+              name="task_title"
+              id="task_title"
+              placeholder="Task title"
+              onChange={(e) => setTaskTitle(e.target.value)}
+              className="bg-slate-600 w-3/4 h-full p-3 rounded-lg text-2xl text-slate-300 outline-none focus:ring-2 focus:ring-slate-500 transition-all duration-300 ease-in-out"
+            />
+            <input
+              type="text"
+              name="task_desc"
+              id="task_desc"
+              placeholder="Task description"
+              onChange={(e) => setTaskDesc(e.target.value)}
+              className="bg-slate-600 w-3/4 h-full p-3 rounded-lg text-2xl text-slate-300 outline-none focus:ring-2 focus:ring-slate-500 transition-all duration-300 ease-in-out"
+            />
+          </div>
+
           <button
             type="submit"
-            className="w-1/4 bg-black text-white text-2xl p-3 rounded-lg hover:bg-slate-700 transition-all duration-300 ease-in-out"
+            className="w-1/4 bg-black text-white text-2xl p-3 rounded-lg hover:bg-slate-700 focus:ring-2 focus:ring-slate-500 transition-all duration-300 ease-in-out"
           >
             Add
           </button>
@@ -29,7 +71,6 @@ export default function Home() {
       </form>
 
       {/* tasks list */}
-      
       <Tasks />
     </main>
   );
--- components/Task.tsx
+++ components/Task.tsx
@@ -1,32 +1,42 @@
 import { ITask } from "@/Interfaces/ITask";
 import React from "react";
 import TaskCard from "./TaskCard";
+import { useAppSelector } from "@/redux/hooks";
+import { selectTasks } from "@/redux/features/taskSlice";
 
 type Props = {};
-const tasks: ITask[] = [
-  { title: "task 1", description: "Home stuff", id: 1, isCompleted: false },
-  { title: "task 1", description: "Home stuff", id: 2, isCompleted: false },
-  { title: "task 1", description: "Home stuff", id: 3, isCompleted: false },
-  { title: "task 1", description: "Home stuff", id: 3, isCompleted: false },
-  { title: "task 1", description: "Home stuff", id: 3, isCompleted: false },
-  { title: "task 1", description: "Home stuff", id: 3, isCompleted: false },
-];
+// const tasks: ITask[] = [
+//   { title: "task 1", description: "Home stuff", id: 1, isCompleted: false },
+//   { title: "task 1", description: "Home stuff", id: 2, isCompleted: false },
+//   { title: "task 1", description: "Home stuff", id: 3, isCompleted: false },
+//   { title: "task 1", description: "Home stuff", id: 4, isCompleted: false },
+//   { title: "task 1", description: "Home stuff", id: 5, isCompleted: false },
+//   { title: "task 1", description: "Home stuff", id: 6, isCompleted: false },
+// ];
 
 const Tasks = (props: Props) => {
+  const { items: tasks, error } = useAppSelector(selectTasks);
+  console.log(tasks[0] ? tasks[0] : "no task");
   return (
-    <div className="h-3/6 w-1/2 mx-auto space-y-3  ">
+    <div className="h-3/6 w-1/2 flex flex-col mx-auto space-y-3  ">
       <h1 className="text-2xl capitalize font-bold">Tasks</h1>
-      <div className="h-3/4 overflow-y-scroll no-scrollbar snap">
-        {tasks.map((task) => (
-          <TaskCard
-            key={task.id}
-            id={task.id}
-            title={task.title}
-            description={task.description}
-            isCompleted={task.isCompleted}
-          />
-        ))}
-      </div>
+      {tasks[0] ? (
+        <div className="h- overflow-y-scroll no-scrollbar snap">
+          {tasks.map((task, index) => (
+            <TaskCard
+              key={task.id}
+              id={task.id}
+              title={task.title}
+              description={task.description}
+              isCompleted={task.isCompleted}
+            />
+          ))}
+        </div>
+      ) : (
+        <div className="text-2xl text-white bg-gray-500 w-1/2 mx-auto uppercase text-center p-6 rounded-lg font-bold">
+          No task
+        </div>
+      )}
     </div>
   );
 };
